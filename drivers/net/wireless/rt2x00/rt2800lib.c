89afb6c7d67087b5f8de39d3e367291acac2a36d
popcornmix <popcornmix@gmail.com>
2013-09-07 19:07:33 +0100

    Support for cheap Ralink 3070 WiFi plug
    See: https://github.com/raspberrypi/linux/pull/321

--- drivers/net/wireless/rt2x00/rt2800lib.c
+++ drivers/net/wireless/rt2x00/rt2800lib.c
@@ -2599,6 +2599,7 @@ static void rt2800_config_channel(struct rt2x00_dev *rt2x00dev,
 		break;
 	case RF5360:
 	case RF5370:
+	case RF3070:
 	case RF5372:
 	case RF5390:
 	case RF5392:
@@ -2615,6 +2616,7 @@ static void rt2800_config_channel(struct rt2x00_dev *rt2x00dev,
 	    rt2x00_rf(rt2x00dev, RF3322) ||
 	    rt2x00_rf(rt2x00dev, RF5360) ||
 	    rt2x00_rf(rt2x00dev, RF5370) ||
+	    rt2x00_rf(rt2x00dev, RF3070) ||
 	    rt2x00_rf(rt2x00dev, RF5372) ||
 	    rt2x00_rf(rt2x00dev, RF5390) ||
 	    rt2x00_rf(rt2x00dev, RF5392)) {
@@ -3199,6 +3201,7 @@ void rt2800_vco_calibration(struct rt2x00_dev *rt2x00dev)
 	case RF3290:
 	case RF5360:
 	case RF5370:
+	case RF3070:
 	case RF5372:
 	case RF5390:
 	case RF5392:
@@ -5515,6 +5518,7 @@ static int rt2800_init_eeprom(struct rt2x00_dev *rt2x00dev)
 	case RF3322:
 	case RF5360:
 	case RF5370:
+	case RF3070:
 	case RF5372:
 	case RF5390:
 	case RF5392:
@@ -5970,6 +5974,7 @@ static int rt2800_probe_hw_mode(struct rt2x00_dev *rt2x00dev)
 		   rt2x00_rf(rt2x00dev, RF3322) ||
 		   rt2x00_rf(rt2x00dev, RF5360) ||
 		   rt2x00_rf(rt2x00dev, RF5370) ||
+		   rt2x00_rf(rt2x00dev, RF3070) ||
 		   rt2x00_rf(rt2x00dev, RF5372) ||
 		   rt2x00_rf(rt2x00dev, RF5390) ||
 		   rt2x00_rf(rt2x00dev, RF5392)) {
@@ -6072,6 +6077,7 @@ static int rt2800_probe_hw_mode(struct rt2x00_dev *rt2x00dev)
 	case RF3290:
 	case RF5360:
 	case RF5370:
+	case RF3070:
 	case RF5372:
 	case RF5390:
 	case RF5392:
